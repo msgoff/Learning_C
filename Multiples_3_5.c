664bcfd4f10ed5902724f84e227601dfb7dbdc4b
Michael Goff <msgoff@users.noreply.github.com>
2018-03-15 16:20:02 +0000

    Update Multiples_3_5.c

--- Multiples_3_5.c
+++ Multiples_3_5.c
@@ -1,16 +1,20 @@
-#include "stdio.h"
-/* find the sum of all numbers that are divisible by 3 or 5 less than 1000 */
-int main(void) {
-   int i;
-   int total = 0 ;
-   int var;
-   
-   for (i=1;i<1000;i++){
-   if (i % 3 == 0 || i % 5 == 0){
-   	total += i;
-   }
-   }
-   
-   	printf("%d\n",total);
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[]){
+        printf("%s \n","print the sum of 3's and 5's less than the paramater passed");
+        int total = 0;
+        int i = 0;
+        int passed_parameter = atoi(argv[1]);
+
+        for (i=0;i<passed_parameter;i++){
+                if ( i%3 == 0 || i%5 == 0 ){
+                        total += i;
+                }
+        }
+
+        printf("%d \n",total);
+         
+        return 0;
 }
+
