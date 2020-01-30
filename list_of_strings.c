91602fb76a329a967b066657bedf0de5fb5f5007
Michael Goff <msgoff@users.noreply.github.com>
2020-01-30 00:47:53 +0000

    Add files via upload

---
+++ list_of_strings.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  list_of_strings.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2020 12:45:34 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include	<stdio.h>
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	char *list[] = {"this","is a","test"};
+	int i = 0;
+	for(i=0;i<3;i++){
+		printf("%s\n",list[i]);
+	}
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
